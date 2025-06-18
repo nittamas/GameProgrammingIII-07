afac39551c09cd74e385960660eeae7bfdaa30d0
Masahiro Nitta <83905521+nittamas@users.noreply.github.com>
2025-06-19 01:27:44 +0900

    アセットの追加

---
+++ Assets/Scrripts/Player.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using R3;
+
+public class Player : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
